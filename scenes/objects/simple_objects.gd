ee395185fe18d5c40163d069b0e1c110e4818e62
OsamaNajeeb <osamanajeeb59@gmail.com>
2026-08-13 22:53:22 -0700

    Well I learned Tool but IDK it will be useful o algo

--- scenes/objects/simple_objects.gd
+++ scenes/objects/simple_objects.gd
@@ -1,16 +1,26 @@
 @tool
 extends StaticBody2D
 
+#Ok gemini the code is working but how does Godot knows that it
+@export var randomBool : bool = true
+
 @export_range(0,3,1) var size: int:
 	set(value):
 		size = value
-		$Sprite2D.frame_coords = Vector2i(size, style)
+		# SAFETY CHECK: Only talk to the Sprite if it exists!
+		if is_node_ready():
+			$Sprite2D.frame_coords = Vector2i(size, style)
+			
 @export_enum('Bush','Rock') var style: int:
 	set(value):
 		style = value
-		$Sprite2D.frame_coords = Vector2i(size, style)
+		# SAFETY CHECK: Only talk to the Sprite if it exists!
+		if is_node_ready():
+			$Sprite2D.frame_coords = Vector2i(size, style)
 		
-		
+@export_tool_button('Randomize Button', "Callable") var  randomzier = randomize
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	#rotate(5 * delta)
@@ -18,4 +28,19 @@ func _process(_delta: float) -> void:
 	pass
 
 func _ready() -> void:
+	if randomBool == true:
+		size = randi_range(0,3)
+		style = randi_range(0,1)
+		
 	$Sprite2D.frame_coords = Vector2i(size,style)
+	$CollisionShape2D.disabled = size < 2
+	z_index = -1 if size < 2 else 0
+	
+func randomize():
+	size = randi_range(0,3)
+	style = randi_range(0,1)
+	
+	# You should also add the safety check to your custom Randomize button 
+	# just in case you click it at a weird time in the editor!
+	if is_node_ready():
+		$Sprite2D.frame_coords = Vector2i(size,style)
